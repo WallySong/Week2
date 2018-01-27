258484d67393bf4901d78ea7212df2ee0905ecf7
WallySong <bio950327@naver.com>
2018-01-27 14:35:06 +0900

    my first commit

---
+++ app/src/main/java/com/example/samsung/myapplication/Study01.java
@@ -0,0 +1,13 @@
+package com.example.samsung.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Study01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study01);
+    }
+}
